6a5dc3513ca0e9e89160f622240d1d1276f3127d
lukas-zip <lukas.huberx1999@gmail.com>
2024-03-22 16:01:43 +0100

    update dynamodb functions and routes

--- backend/user-service/app/__init__.py
+++ backend/user-service/app/__init__.py
@@ -4,9 +4,8 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    dynamodb.create_profiles_table()
-    dynamodb.create_email_index()
-    dynamodb.add_dummy_users()
+    dynamodb.create_user_management_tables()
+    dynamodb.add_dummy_data()
     CORS(app)
     return app
 
--- backend/user-service/app/dynamodb.py
+++ backend/user-service/app/dynamodb.py
@@ -2,7 +2,7 @@ import boto3
 import uuid
 from botocore.exceptions import ClientError
 
-db_users = boto3.client(
+db_user_management = boto3.client(
     "dynamodb",
     aws_access_key_id="test",
     aws_secret_access_key="test",
@@ -10,100 +10,179 @@ db_users = boto3.client(
     endpoint_url="http://localstack:4566"
 )
 
+
 # Function to create the profiles table
-def create_profiles_table():
+def create_user_management_tables():
     try:
-        response = db_users.create_table(
-            TableName='UserProfiles',
+        response = db_user_management.create_table(
+            TableName='UserManagement',
             KeySchema=[
-                {'AttributeName': 'profile_id', 'KeyType': 'HASH'}  # Partition key
+                {'AttributeName': 'PK', 'KeyType': 'HASH'},
+                {'AttributeName': 'SK', 'KeyType': 'RANGE'}
             ],
             AttributeDefinitions=[
-                {'AttributeName': 'profile_id', 'AttributeType': 'S'}  # String
+                {'AttributeName': 'PK', 'AttributeType': 'S'},
+                {'AttributeName': 'SK', 'AttributeType': 'S'},
+                {'AttributeName': 'Email', 'AttributeType': 'S'}
             ],
-            ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5}
-        )
-        print("UserProfiles table created:", response)
-    except ClientError as e:
-        print("Error creating UserProfiles table:", e)
-
-
-def create_email_index():
-    try:
-        response = db_users.update_table(
-            TableName='UserProfiles',
-            AttributeDefinitions=[
-                {'AttributeName': 'email', 'AttributeType': 'S'}
-            ],
-            GlobalSecondaryIndexUpdates=[
+            ProvisionedThroughput={'ReadCapacityUnits': 10, 'WriteCapacityUnits': 10},
+            GlobalSecondaryIndexes=[
                 {
-                    'Create': {
-                        'IndexName': 'email-index',
-                        'KeySchema': [
-                            {'AttributeName': 'email', 'KeyType': 'HASH'}
-                        ],
-                        'Projection': {
-                            'ProjectionType': 'ALL'  # Projection type can be adjusted based on your needs
-                        },
-                        'ProvisionedThroughput': {
-                            'ReadCapacityUnits': 5,
-                            'WriteCapacityUnits': 5
-                        }
+                    'IndexName': 'EmailIndex',
+                    'KeySchema': [
+                        # Email will be the partition key for the GSI
+                        {'AttributeName': 'Email', 'KeyType': 'HASH'}
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'KEYS_ONLY'
                     }
                 }
             ]
         )
-        print("Global secondary index created:", response)
+        print("EcommercePlatform table created:", response)
     except ClientError as e:
-        print("Error creating global secondary index:", e)
+        print("Error creating EcommercePlatform table:", e)
+
 
 # Function to get a user by email
-def get_user_by_email(email):
-    try:
-        response = db_users.query(
-            TableName='UserProfiles',
-            IndexName='email-index',  # Assuming 'email-index' is the name of your GSI
-            KeyConditionExpression='email = :email',
-            ExpressionAttributeValues={':email': {'S': email}}
-        )
-        items = response.get('Items', [])
-        if items:
-            return items[0]
-        else:
-            return None
-    except ClientError as e:
-        print("Error getting user by email:", e)
-        return None
+# def get_user_by_email(email):
+#     try:
+#         response = db_users.query(
+#             TableName='UserProfiles',
+#             IndexName='email-index',  # Assuming 'email-index' is the name of your GSI
+#             KeyConditionExpression='email = :email',
+#             ExpressionAttributeValues={':email': {'S': email}}
+#         )
+#         items = response.get('Items', [])
+#         if items:
+#             return items[0]
+#         else:
+#             return None
+#     except ClientError as e:
+#         print("Error getting user by email:", e)
+#         return None
 
 # Function to add a user
-def add_user(email, password):
+def add_user(email, password, username):
     try:
-        # Check if the user already exists
-        if get_user_by_email(email):
-            print(f"User with email {email} already exists")
-            return
+        if user_in_db(email) is not None:
+            return {'error': 'Email already in use by a different profile'}
 
         # Generate UUID for the new user
         user_uuid = str(uuid.uuid4())
 
         # Put the new item into the table
-        response = db_users.put_item(
-            TableName='UserProfiles',
+        response = db_user_management.put_item(
+            TableName='UserManagement',
             Item={
-                'profile_id': {'S': user_uuid},  # Automatically generated UUID
-                'email': {'S': email},  # User's email address
-                'password': {'S': password}  # User's password (should be hashed in production)
+                'PK': {'S': f'USER#{user_uuid}'},
+                'SK': {'S': f'PROFILE#{user_uuid}'},
+                'Type': {'S': 'User'},
+                'Email': {'S': email},
+                'Password': {'S': password},
+                'Username': {'S': username}
             }
         )
         print("User added with UUID:", user_uuid)
+        return user_uuid
     except ClientError as e:
         print("Error adding user:", e)
 
+
+def user_in_db(email):
+    try:
+        response = db_user_management.query(
+            TableName='UserManagement',
+            IndexName='EmailIndex',  # Replace with the actual name of your GSI
+            KeyConditionExpression='Email = :email',
+            ExpressionAttributeValues={':email': {'S': email}}
+        )
+        return response['Items'][0] if response['Items'] else None
+    except ClientError as e:
+        print(f"Error checking email existence: {e}")
+        return None  # Assuming that if there's an error, the check is inconclusive
+
+
+# Function to get a user by UUID
+def get_user(user_uuid):
+    try:
+        response = db_user_management.get_item(
+            TableName='UserManagement',
+            Key={
+                'PK': {'S': f'USER#{user_uuid}'},
+                'SK': {'S': f'PROFILE#{user_uuid}'}
+            }
+        )
+        return response.get('Item')
+    except ClientError as e:
+        print("Error getting user:", e)
+
+
+def add_shop(shop_name, email, password, address, phone):
+    try:
+        # Check if the user already exists
+        if get_user(email):
+            print(f"User with email {email} already exists")
+            return
+
+        # Generate UUID for the new user
+        shop_uuid = str(uuid.uuid4())
+
+        # Put the new item into the table
+        response = db_user_management.put_item(
+            TableName='UserManagement',
+            Item={
+                'PK': {'S': f'SHOP#{shop_uuid}'},
+                'SK': {'S': f'DETAILS#{shop_uuid}'},
+                'Type': {'S': 'Shop'},
+                'Email': {'S': email},
+                'Password': {'S': password},
+                'ShopName': {'S': shop_name},
+                'Address': {'S': address},
+                'Phone': {'S': phone}
+            }
+        )
+        print("Shop added with ID:", shop_uuid)
+        return shop_uuid
+    except ClientError as e:
+        print("Error adding shop:", e)
+
+
+# Function to get a shop by ID
+def get_shop(shop_id):
+    try:
+        response = db_user_management.get_item(
+            TableName='UserManagement',
+            Key={
+                'PK': {'S': f'SHOP#{shop_id}'},
+                'SK': {'S': f'DETAILS#{shop_id}'}
+            }
+        )
+        return response.get('Item')
+    except ClientError as e:
+        print("Error getting shop:", e)
+
+
 # Function to add dummy users
-def add_dummy_users():
+def add_dummy_data():
     dummy_users = [
-        {"email": "felix@muster.com", "password": "123"},
-        {"email": "thomas@muster.com", "password": "456"}
+        {"email": "john.doe@example.com", "password": "password1", "username": "johndoe"},
+        {"email": "jane.doe@example.com", "password": "password2", "username": "janedoe"},
+        {"email": "max.smith@example.com", "password": "password3", "username": "maxsmith"}
     ]
+    # Adding dummy users
     for user in dummy_users:
-        add_user(user["email"], user["password"])
+        add_user(email=user["email"], password=user["password"], username=user["username"])
+
+    dummy_shops = [
+        {"shop_name": "John Micro", "email": "micro@example.com", "password": "password11",
+         "address": "Hoferstrasse 19, Zuerich", "phone": "+41 1234567890"},
+        {"shop_name": "Jane Hydro", "email": "hydro@example.com", "password": "password22",
+         "address": "Bernerstrasse 18, Bern", "phone": "+41 2395678901"},
+        {"shop_name": "Max Electro", "email": "electro@example.com", "password": "password33",
+         "address": "Weinhofstrasse 123, Luzern", "phone": "+41 3456789012"}
+    ]
+    # Adding dummy shops
+    for shop in dummy_shops:
+        add_shop(shop_name=shop["shop_name"], email=shop["email"], password=shop["password"], address=shop["address"],
+                 phone=shop["phone"])
