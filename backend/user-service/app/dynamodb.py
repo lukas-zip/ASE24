8a0d359540193a4cab3d47a9f3f517f058ee25b3
lukas-zip <lukas.huberx1999@gmail.com>
2024-03-24 16:20:26 +0100

    user profile upload kind of working. still have to adapt it for shop, bugfixes, extend comments

--- backend/user-service/app/dynamodb.py
+++ backend/user-service/app/dynamodb.py
@@ -1,6 +1,13 @@
 import boto3
 import uuid
 from botocore.exceptions import ClientError
+import os
+from werkzeug.utils import secure_filename
+from botocore.exceptions import ClientError
+from flask import Flask, Response
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 db_user_management = boto3.client(
     "dynamodb",
@@ -9,6 +16,22 @@ db_user_management = boto3.client(
     region_name="us-east-1",
     endpoint_url="http://localstack:4566"
 )
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id="test",  # Dummy Access Key
+    aws_secret_access_key="test",  # Dummy Secret Key
+    region_name="us-east-1",  # or your LocalStack configuration's region
+    endpoint_url="http://localstack:4566"  # URL for LocalStack (adjust if using a different port)
+)
+
+# Create S3 bucket on LocalStack
+def create_s3_bucket():
+    try:
+        bucket_name = 'userview'
+        s3_client.create_bucket(Bucket=bucket_name)
+        print(f"Bucket {bucket_name} created successfully.")
+    except ClientError as e:
+        print(f"Error creating bucket: {e}")
 
 
 # Function to create the profiles table
@@ -53,12 +76,13 @@ def add_user(email, password, username):
         user_uuid = str(uuid.uuid4())
 
         # Put the new item into the table
-        response = db_user_management.put_item(
+        db_user_management.put_item(
             TableName='UserManagement',
             Item={
                 'PK': {'S': f'USER#{user_uuid}'},
                 'SK': {'S': f'PROFILE#{user_uuid}'},
                 'type': {'S': 'User'},
+                'profile_picture': {'S': "NONE"},
                 'email': {'S': email},
                 'username': {'S': username},
                 'password': {'S': password}
@@ -103,8 +127,8 @@ def user_in_db(email):
 def add_shop(shop_name, email, password, address, phone):
     try:
         # Check if the user already exists
-        if get_user(email):
-            print(f"User with email {email} already exists")
+        if user_in_db(email):
+            print(f"Email already in use by a different profile")
             return
 
         # Generate UUID for the new user
@@ -131,15 +155,74 @@ def add_shop(shop_name, email, password, address, phone):
 
 
 # Function to get a shop by ID
-def get_shop(shop_id):
+def get_shop(shop_uuid):
+    """
+    Retrieve a shop's details from the DynamoDB table based on its UUID.
+
+    :param shop_uuid: The UUID of the shop to retrieve.
+    :return: A dictionary representing the shop item if found, None otherwise.
+    """
     try:
         response = db_user_management.get_item(
             TableName='UserManagement',
             Key={
-                'PK': {'S': f'SHOP#{shop_id}'},
-                'SK': {'S': f'DETAILS#{shop_id}'}
+                'PK': {'S': f'SHOP#{shop_uuid}'},
+                'SK': {'S': f'DETAILS#{shop_uuid}'}
             }
         )
         return response.get('Item')
     except ClientError as e:
         print("Error getting shop:", e)
+
+
+def get_entity(entity_uuid):
+    """
+    Fetch an entity (user or shop) from the DynamoDB table using the entity UUID.
+
+    :param entity_uuid: The UUID of the entity to fetch.
+    :return: The entity item if found, None otherwise.
+    """
+    try:
+        # Attempt to fetch the user
+        user_response = get_user(entity_uuid)
+        if user_response is not None and 'Item' in user_response:
+            return user_response['Item']
+
+        # If not found, attempt to fetch the shop
+        shop_response = get_shop(entity_uuid)
+        if shop_response is not None and 'Item' in shop_response:
+            return shop_response['Item']
+        # If no entity is found, return None
+        return None
+    except ClientError as e:
+        print("Error getting entity:", e)
+
+
+# Uploads a profile picture to S3 and updates the user's DynamoDB entry.
+def update_profile_picture(entity_uuid, file_path, filename):
+    try:
+        bucket_name = 'userview'
+        s3_client.upload_file(file_path, bucket_name, f"{entity_uuid}/{filename}")
+        image_url = f"http://{bucket_name}.s3.localhost.localstack.cloud:4566/{entity_uuid}/{filename}"
+
+        # Determine the prefix based on the entity type
+        entity_type = get_user(entity_uuid)['type']
+        prefix = 'USER#' if entity_type.get('S') == 'User' else 'SHOP#'
+
+        # Update the item in the table to include the profile picture URL
+        response = db_user_management.update_item(
+            TableName='UserManagement',
+            Key={
+                'PK': {'S': f'{prefix}{entity_uuid}'},
+                'SK': {'S': f'PROFILE#{entity_uuid}' if entity_type.get('S') == 'User' else f'DETAILS#{entity_uuid}'},
+            },
+            UpdateExpression='SET profile_picture = :val',
+            ExpressionAttributeValues={
+                ':val': {'S': image_url}
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        print(f"{entity_type} profile picture updated:", response)
+        return response
+    except ClientError as e:
+        print(f"Error updating profile picture:", e)
--- backend/user-service/app/routes.py
+++ backend/user-service/app/routes.py
@@ -1,14 +1,15 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import boto3
+from flask import Flask, jsonify, request, send_file
 from app import app, dynamodb
-import uuid
+from werkzeug.utils import secure_filename
+import os
 from botocore.exceptions import ClientError
 
 # Test if endpoint is available
-@app.route('/', methods=['GET'])
+@app.route('/t2', methods=['GET'])
 def test():
     # Return success response
+    app.logger.info('Info level log')
+    print("Hello, world!")
     return jsonify({'message': 'Test successful'}), 201
 
 # User registration by retrieving data from post request and saving into dynamodb
@@ -65,3 +66,36 @@ def login():
             return jsonify({'error': 'Invalid login credentials'}), 401
     except ClientError as e:
         return jsonify({'error': str(e)}), 500
+
+@app.route('/profile/<entity_uuid>', methods=['POST'])
+def update_profile_picture(entity_uuid):
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file:
+        # Secure the filename and prepend "profile" to it
+        filename = secure_filename(file.filename)
+        _, extension = os.path.splitext(filename)
+        new_filename = f"profile_picture{extension}"
+        # Set up the upload folder
+        upload_folder = './backend/user-service'
+        os.makedirs(upload_folder, exist_ok=True)
+        # Construct the full file path
+        file_path = os.path.join(upload_folder, new_filename)
+        # Save the file locally
+        file.save(file_path)
+        profile_picture_url = dynamodb.update_profile_picture(entity_uuid, file_path, new_filename)
+        os.remove(file_path)
+        return jsonify({'message': 'File uploaded successfully', 'url': profile_picture_url}), 200
+    return jsonify({'message': 'File uploaded unsuccessfull'}), 401
+
+
+@app.route('/user/<user_uuid>', methods=['POST'])
+def test_user(user_uuid):
+    test = dynamodb.get_user(user_uuid)
+    return jsonify({'error': 'No file part', 'Entity': test}), 200
