cde9d338206634ea4d10d0acac277f6b741592f7
lenanf <lena-fischer@gmx.de>
2024-05-04 11:12:54 +0200

    Adding test scenarios to the unit-test

--- backend/reviews/tests/review_test.py
+++ backend/reviews/tests/review_test.py
@@ -15,48 +15,49 @@ def test_add_review(client):
         "reviewcontent": "Greate Design.",
         "rating": "5"
     }
-    # POST request to '/users' to register a new user
+    # POST request to '/review' to register a new review
     response = client.post('/review', data=json.dumps(review_data), content_type='application/json')
     # Check if the request was successful
     assert response.status_code == 200
     assert response.json['status'] == True
     assert response.json['value'] == "Successfully added Review"
 
-def test_edit_review(client):
+    review2_data = {
+        "customer_id": "4321", 
+        "reviewcontent": "Greate Design.",
+        "rating": "5"
+    }
+    # POST request to '/review' to register a new review
+    response = client.post('/review', data=json.dumps(review2_data), content_type='application/json')
+    # Check if the request was successful
+    assert response.status_code == 400
+    assert response.json['status'] == False
+    assert response.json['message'] == "Product_ID, Customer_ID and rating are required!"
+
+def test_check_review(client):
     review_data = {
         "product_id": "1234",
         "customer_id": "4321", 
         "reviewcontent": "Greate Design.",
         "rating": "5"
     }
-    # POST request to '/users' to register a new user
-    #client.post('/review', data=json.dumps(review_data), content_type='application/json')
-    response = client.get('/review/1234')
+    check_data = {
+        "product_id": "1234",
+        "customer_id": "4321"        
+    }
+    # Check the extistance of a review for that user and product
+    response = client.get('/review/check', data=json.dumps(check_data), content_type='application/json')
+    # Check if the request was successful
+    assert response.status_code == 200
+    assert response.json['status'] == True
+    assert response.json['value'] == "Customer has not jet made a review for this product"
+    
+    # POST request to '/review' to register a new review
+    client.post('/review', data=json.dumps(review_data), content_type='application/json')
+
+    # Check the extistance of a review for that user and product
+    response = client.get('/review/check', data=json.dumps(check_data), content_type='application/json')
+    # Check if the request was successful
     assert response.status_code == 200
     assert response.json['status'] == False
-    #response_value = response.json['value'][0]
-    #assert response_value['review_id'] != ""
-    #review_id = response_value['review_id']
-
-    #review_newdata = {
-    #    "product_id": "1234",
-    #    "customer_id": "4321", 
-    #    "reviewcontent": "Good Design.",
-    #    "rating": "4",
-    #    "review_id": review_id
-    #}
-    
-    #response = client.put('/users', data=json.dumps(review_newdata), content_type='application/json')
-    #response_get = client.get('/review/1234')
-    #assert response_get.status_code == 200
-    #response_getjs = response_get.json['value']
-    #response_value = response_getjs[0]
-    #assert response_value['review_id'] == review_id
-    #assert response_value['product_id'] == review_newdata['product_id']
-    #assert response_value['customer_id'] == review_newdata['customer_id']
-    #assert response_value['reviewcontent'] == review_newdata['reviewcontent']
-    #assert response_value['rating'] == review_newdata['rating']
-
-#def test_delete_review(client):
-#    assert None
-
+    assert response.json['message'] == "Review already exists"
