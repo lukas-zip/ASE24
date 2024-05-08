aa7c91862dc2afdde9aa781559ebb7f03e3aa735
lenanf <56082694+lenanf@users.noreply.github.com>
2024-05-08 10:02:05 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 docker-compose -f docker-compose.test.yml up -d
 docker-compose logs --tail=1000 -f user-service
-docker-compose logs --tail=1000 -f financial-service
 docker-compose logs --tail=1000 -f reviews
+docker-compose logs --tail=1000 -f financial-service
 docker-compose down
 
 
